4f4cf3d4609882f739451a82624e958516177eb5
Ishan Manchanda <ishanmanchanda70@gmail.com>
2020-05-19 20:33:07 +0530

    Added tests scaffolding

---
+++ bootstrap_components/tests/test_render.py
@@ -0,0 +1,10 @@
+from django.urls import reverse
+
+
+def test_render(client):
+	# TODO: uncomment when demo URL active
+	# url = reverse('demo')
+	# resp = client.get(url, secure=True)
+	# assert resp.status_code == 200
+	# TODO: assert template rendered successfully
+	pass
---
+++ bootstrap_components/tests/urls.py
@@ -0,0 +1,7 @@
+# TODO: Figure out how to map a URL specifically for testing
+
+# TODO: Use a generic template view to render the demo template
+
+# REVIEW: Where is the ideal location to place the demo template?
+#  The templates directory under a new namespace?
+#  A Different templates directory inside the tests folder?
